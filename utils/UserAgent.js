0c1438e880dd853a83013b4bfa899000ecf0167e
Tycho Bokdam <tycho@palmtreecoding.com>
2019-10-08 15:40:35 +0200

    fix: Import webview from react-native-webview

--- utils/UserAgent.js
+++ utils/UserAgent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, WebView } from 'react-native';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export const UserAgent = ({setUserAgent}) => {
 	const content = `
