7bce38e4b6d9443687ffbd88fd4c24717e30434e
Anwar Jebali <anwar.j@me.com>
2021-05-20 10:47:34 +0200

    New import for TextFielld

--- components/TextBlock.js
+++ components/TextBlock.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import PropTypes from 'prop-types';
-import { TextField } from '@ubaids/react-native-material-textfield';
+import { TextField } from 'rn-material-ui-textfield';
 import ErrorMessage from './ErrorMessage';
 import withTheme from '../core/withTheme'
 
