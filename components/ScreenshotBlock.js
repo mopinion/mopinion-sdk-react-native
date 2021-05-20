3c7ec5705b24e4b1a501fce37c1292861da5b098
Anwar Jebali <anwar.j@me.com>
2021-05-20 10:50:52 +0200

    Implement react-native-image-picker for uploading custom images in ScreenshotBlock through conditional import

--- components/ScreenshotBlock.js
+++ components/ScreenshotBlock.js
@@ -1,21 +1,169 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Dimensions, Text, View, StyleSheet, Image, LayoutAnimation } from 'react-native';
+import { Alert, Dimensions, Modal, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import Control from './Control';
 import ErrorMessage from './ErrorMessage';
+import Button from './Button';
+
+
+// to select the default screenshot or an image from the photo library
+// if the user selects an image, the button changes face to revert to default screenshot
+class ImagePickerControl extends React.Component {
+      
+  static defaultProps = {
+    pickLabel:'Pick',
+    undoLabel:'Undo',
+    maxImageWidth:320,
+    maxImageHeight:480,
+    visible:false
+  };
+
+  constructor(props) {
+	super(props);
+	
+	this.state = {
+		enableUndo:false,
+		imagePickerResponse:null
+	};
+
+  }
+
+	updateParentState() {
+		// callback to pass the selected image type and uri 
+		if (typeof this.props.onImagePicked === 'function') {
+			if(this.state.imagePickerResponse != null) {
+				this.props.onImagePicked(this.state.imagePickerResponse.type, 
+										this.state.imagePickerResponse.uri, 
+										this.state.imagePickerResponse.base64);
+			} else {
+				this.props.onImagePicked(null, null, null);
+			}
+		}
+	}
+	
+	setImagePickerResponse(response) {
+	    this.setState((prevState) => {
+      return {
+          ...prevState,
+          imagePickerResponse:response
+      }
+    },() => {
+      this.updateParentState();
+    });
+	}
+
+	// update the button state after the user picked an image or did undo
+	toggleButtonFace() {
+	    this.setState((prevState) => {
+		  return {
+			  ...prevState,
+			  enableUndo:!prevState.enableUndo
+		  }
+		});
+	}
+	
+	validatePickedImage(response) {
+		if(response && response.uri !== "" ) {
+			switch(response.type) {
+				case 'image/png':
+				case 'image/jpg':
+					break;
+				default:
+					Alert.alert('Unsupported Image', `Format '${response.type}' is not supported. Please select a JPG or PNG image.`);
+					return false;
+			}
+		}
+		return true;
+	}
+	
+  // handle the pick/undo button
+  pressHandler(key,value,data) {
+	// the button is either in undo or imagepicker mode
+    if(this.state.enableUndo) {
+      // Undo, revert to the default screenshot and toggle button face
+      this.toggleButtonFace();
+      this.setImagePickerResponse(null);
+    } else {
+      import('react-native-image-picker').then(ImagePicker => {
+        ImagePicker.launchImageLibrary(
+          {
+          mediaType: 'photo',
+          includeBase64: true,
+          maxHeight: this.props.maxImageHeight,
+          maxWidth: this.props.maxImageWidth,
+          },
+          (response) => {
+            // process the response for the right type
+            if(response && response.uri !== ""  && response.didCancel !== true && this.validatePickedImage(response) ) {
+            this.toggleButtonFace();
+
+            this.setImagePickerResponse(response);
+            }
+          },
+        );
+      })
+    }
+  }
+  
+  render() {
+	const { data } = this.props;
+	const styles = StyleSheet.create({
+	  button: {
+		height:36,
+		paddingHorizontal:3,
+		alignItems:'center',
+		justifyContent:'center',
+		flexDirection:'row',
+		marginTop:10
+	  },
+	});
+
+      let obj = this.state;
+	  let buttonProps = {
+		text: obj.enableUndo ? this.props.undoLabel : this.props.pickLabel,
+		raised:false,
+	    onPress:this.pressHandler.bind(this, 'picked_image', obj.enableUndo,data),
+		border:true,
+		buttonType:'toggle',
+		icon: obj.enableUndo ? 'undo' : 'pictureO'
+	  };
+
+	return (
+	  this.props.visible ?
+	  (
+		<View style={styles.button}>
+			<Button {...buttonProps} />
+		</View>
+	  )
+	  : null
+	);
+  }
+}
 
 export default class ScreenshotBlock extends React.Component {
 
   static defaultProps = {
-    screenshotCheckboxLabel:'Send screenshot with feedback?'
+    screenshotCheckboxLabel:'Send screenshot with feedback?',
+    screenshotImageType:'image/png'
   };
 
   constructor(props) {
     super(props);
+    this.thumbnailRatio = 0.25; // in relation to the height of the display
+    this.overlayImageRatio = 0.9;  // in relation to display width or height
+
+    let maxWidth = Dimensions.get('window').width * this.thumbnailRatio;
+    let maxHeight = Dimensions.get('window').height * this.thumbnailRatio;
+
     this.state = {
       imageWidth:0,
       imageHeight:0,
-      sizeSet:false
+      thumbnailWidth:maxWidth,
+      thumbnailHeight:maxHeight,
+      sizeSet:false,
+      overlayVisible:false,
+		defaultScreenshotImageType: this.props.screenshotImageType,
+		defaultScreenshotData: this.props.formGroupState.screenshot,
+		defaultThumbnailWidth:maxWidth
     };
 
     this.state[this.props.data.typeName] = {
@@ -24,27 +172,26 @@ export default class ScreenshotBlock extends React.Component {
     };
   }
 
-  async UNSAFE_componentWillMount() {
-    this.getSetImageSize()
-  }
+  componentDidMount() {
+    const { formGroupState } = this.props;
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.formGroupState.screenshot && !this.state.sizeSet) {
-      this.getSetImageSize();
+    if (formGroupState.screenshot && !this.state.sizeSet) {
+	  const imageType = formGroupState.screenshotImageType ? formGroupState.screenshotImageType : this.state.defaultScreenshotImageType;
+
+      this.initImageSizes(`data:${imageType};base64,${formGroupState.screenshot}`);
     }
   }
 
-  getSetImageSize() {
+  initImageSizes(imageURI) {
 
-    const { formGroupState } = this.props;
+    if (typeof imageURI === 'string' && imageURI !== '') {
 
-    if (!this.state.sizeSet && typeof formGroupState.screenshot === 'string' && formGroupState.screenshot.length) {
-      Image.getSize(`data:image/png;base64,${formGroupState.screenshot}`, (width,height) => {
-
-        let maxWidth = Dimensions.get('window').width * .8;
-        let maxHeight = Dimensions.get('window').height * .6;
+      Image.getSize(imageURI, (width,height) => {
+        let maxWidth = Dimensions.get('window').width * this.overlayImageRatio;
+        let maxHeight = Dimensions.get('window').height * this.overlayImageRatio;
 
         let imageSize = {};
+        let thumbNailSize = {};
 
         if (height > maxHeight) {
           imageSize.width = width * (maxHeight / height);
@@ -57,26 +204,111 @@ export default class ScreenshotBlock extends React.Component {
           imageSize.height = height;     
         }
 
-        // LayoutAnimation.configureNext({
-        //   duration: 425,
-        //   create: {
-        //     type: LayoutAnimation.Types.spring,
-        //     property: LayoutAnimation.Properties.opacity,
-        //     springDamping: 0.7,
-        //   },
-        //   update: {
-        //     type: LayoutAnimation.Types.spring,
-        //     springDamping: 0.7,
-        //   }
-        // });
+        // determine the scale of the thumbnail in relation the image
+        thumbNailSize.width =  imageSize.width * this.thumbnailRatio / this.overlayImageRatio;
+        thumbNailSize.height =  imageSize.height * this.thumbnailRatio / this.overlayImageRatio;
+        // keep the height of the thumbnail constant, so adjust the width only by the new height
+        if(this.state.thumbnailWidth>0 && thumbNailSize.height>0) {
+          thumbNailSize.width = thumbNailSize.width * this.state.thumbnailHeight / thumbNailSize.height;
+        }
 
         this.setState({
           imageWidth:imageSize.width,
           imageHeight:imageSize.height,
+          thumbnailWidth:thumbNailSize.width,
+          // thumbnailHeight:thumbNailSize.height,
           sizeSet:true
         })
       });
     }
+  }
+
+  setOverlayVisible = (visible) => {
+    this.setState({ overlayVisible: visible });
+  }
+
+  renderOverlay = () => {
+    const { data, formGroupState } = this.props;
+    const { overlayVisible } = this.state;
+    const scaleAsPercentage=100*this.overlayImageRatio;
+
+	const styles = StyleSheet.create({          
+	  overlayImage: {
+		position: 'absolute',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: `${scaleAsPercentage.toFixed(0)}%`,
+		height: `${scaleAsPercentage.toFixed(0)}%`,
+		resizeMode: 'contain'
+	  }, 
+	  modalBackgroundView: {
+		 margin: 20, 
+		 flex: 1, width: '100%', height: '100%', alignItems: "center", justifyContent:"center",
+		 backgroundColor: "#40404080"
+	  },
+	  centeredView: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 30
+	  }
+	});
+
+	const imageType = formGroupState.screenshotImageType ? formGroupState.screenshotImageType : this.state.defaultScreenshotImageType;
+
+	return (
+	  this.state.sizeSet ?
+	  (
+	  <Modal transparent={true} supportedOrientations={['landscape', 'portrait']}
+			  visible={overlayVisible} animationType='fade'
+			  onRequestClose={() => {
+				this.setOverlayVisible(false);
+			  }}
+	  >
+		<View style={styles.centeredView}>
+		  <View style={styles.modalBackgroundView}>
+			<Image
+			  source={{uri:`data:${imageType};base64,${formGroupState.screenshot}`}}
+			  style={styles.overlayImage}
+			/>
+		  </View>
+		</View>
+	  </Modal>
+	  )
+	  : null
+	);
+    
+  };
+
+  renderThumbnail() {
+    const { data, formGroupState } = this.props;
+
+	const imageType = formGroupState.screenshotImageType ? formGroupState.screenshotImageType : this.state.defaultScreenshotImageType;
+	
+    let thumbnailStyle = {
+      flex: 1
+    };
+
+    return(
+      this.state && this.state.sizeSet ?
+      (
+        <TouchableOpacity style={thumbnailStyle} 
+        onPressIn={() => {
+          this.setOverlayVisible(true);
+        }}
+        onPressOut={() => {
+          this.setOverlayVisible(false);
+        }}
+        >
+      <Image
+        source={{uri:`data:${imageType};base64,${formGroupState.screenshot}`}}
+        style={thumbnailStyle}
+        resizeMode={'contain'}
+      />
+      </TouchableOpacity>
+      )
+      : null
+    );
   }
 
   pressHandler(key,value,data) {
@@ -97,18 +329,32 @@ export default class ScreenshotBlock extends React.Component {
     });
   }
 
-  blocks() {
+	// when an image has been picked or reset to default screenshot
+	onImagePicked(imageType, imageURI, imageDataBase64){
+		const updateParentState = () => {
+		  const value = imageDataBase64 ? imageDataBase64 : this.state.defaultScreenshotData;
+		  const newImageType = imageType ? imageType : this.state.defaultScreenshotImageType;
+		  this.props.onFormGroupValueChange({screenshot:value, screenshotImageType:newImageType}, this.props.data);
+		};
 
-    const { data, formGroupState } = this.props;
+		if(imageURI) {
+	      this.initImageSizes(imageURI);
+		} else {
+		  this.setState({ thumbnailWidth:this.state.defaultThumbnailWidth });
+		}	
 
-    let controlProps = {
-      checked:this.state[data.typeName].selected,
-      label:this.props.screenshotCheckboxLabel,
-      value:'send_screenshot',
-      onPress:this.pressHandler.bind(this,data.typeName,'send_screenshot',data),
-      type:'checkbox'
-    };
+		this.setState((prevState) => {
+		  return {
+			  ...prevState,
+			  userPickedImageType:imageType,
+			  userPickedImageURI:imageURI
+		  }
+		},() => {
+		  updateParentState();
+		});
+	}
 
+  screenshotContainer() {
     let screenshotContainerStyle = {
       ...Platform.select({
         ios: {
@@ -122,30 +368,62 @@ export default class ScreenshotBlock extends React.Component {
           backgroundColor: 'white'
         },
       }),
-      height:this.state.imageHeight,
-      width:this.state.imageWidth
+      height:this.state.thumbnailHeight,
+      width:this.state.thumbnailWidth
     };
 
-    let imageStyle = {
-      flex: 1,
-      height:this.state.imageHeight,
-      width:this.state.imageWidth
+    let buttonContainerStyle = {
+      flexDirection: 'column',
+      alignSelf: 'flex-start',
+    };
+
+	const { data, formGroupState } = this.props;
+
+    let controlProps = {
+		data: this.props.data,
+		formGroupState: this.props.formGroupState,
+		onImagePicked: this.onImagePicked.bind(this),
+		maxImageWidth: Dimensions.get('screen').width,
+		maxImageHeight: Dimensions.get('screen').height,
+		visible: data.properties.allow_upload !== 'undefined' ? data.properties.allow_upload : false
+    };
+	
+	// only add these imagepickerlabel props if they exist
+    if (data.properties.pickLabel !== 'undefined') {
+      Object.assign(controlProps, {pickLabel:data.properties.pickLabel})
+    }
+    if (data.properties.undoLabel !== 'undefined') {
+      Object.assign(controlProps, {undoLabel:data.properties.undoLabel})
+    }
+
+  return(
+  	<View>
+      <View style={screenshotContainerStyle}>
+        { this.renderThumbnail() }
+        { this.renderOverlay() }
+      </View>
+      <View style={buttonContainerStyle}>
+        <ImagePickerControl {...controlProps}></ImagePickerControl>
+      </View>
+    </View>
+    );
+  }
+
+  blocks() {
+
+    const { data, formGroupState } = this.props;
+
+    let controlProps = {
+      checked:this.state[data.typeName].selected,
+      label:this.props.screenshotCheckboxLabel,
+      value:'send_screenshot',
+      onPress:this.pressHandler.bind(this,data.typeName,'send_screenshot',data),
+      type:'checkbox'
     };
 
     return (
-      <View 
-      >
-        <View style={screenshotContainerStyle}>
-          {
-            this.state.sizeSet ?
-            (<Image
-              source={{uri:`data:image/png;base64,${formGroupState.screenshot}`}}
-              style={imageStyle}
-              resizeMode={'contain'}
-            />)
-            : null
-          }
-        </View>
+      <View>
+        { this.screenshotContainer() }
         <Control {...controlProps} />
       </View>
     );    
